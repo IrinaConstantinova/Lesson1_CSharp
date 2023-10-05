9ad717dcfc4597253124f4b432fcc178a63032b9
Irina.Constantinova <irina.constantinova@gmail.com>
2023-10-05 14:28:20 +0300

    add Ex5 Conditions if else

---
+++ Ex5_Condition_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter user name, please ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Welcome, Masha!");
+}
+else 
+{
+    Console.Write("Hallo, ");
+    Console.WriteLine(username);
+}
